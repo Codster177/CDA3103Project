3a5a2536cebfb25e66f2975d6b07d2795240cbcd
Codster177 <89281735+Codster177@users.noreply.github.com>
2024-11-22 18:27:52 -0500

    Updated instruction_fetch() and instruction_partition()

--- project.c
+++ project.c
@@ -12,11 +12,11 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-    if (PC % 4)     // Checks for word allignment. (If it is a multiple of 4.)
+    if (PC % 4 == 0)     // Checks for word allignment. (If it is a multiple of 4.)
     {
         //  Assigns the instruction to the instruction at index PC of Mem. Does a bit
         //  adjustment to get the actual location.
-        instruction = Mem[PC>>2];
+        *instruction = Mem[(PC>>2)];
         return 0;   // Because the word allignment was correct, there is no hault condition.
     }
     else
@@ -41,13 +41,13 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
     // When I refer to XOR, it should only get the first n bits to the left of the spot mentioned.
     // n refers to 1<<n.
     // I'm doing this late and need to double check this, but it should do the following:
-    *op = (instruction>>25);    // Only get the bits left of the 25th bit.
-    *r1 = (1<<5 ^ instruction>>20);     // Only get the bits left of the 20th bit, and XOR it.
-    *r2 = (1<<5 ^ instruction>>15);     // Only get the bits left of the 15th bit, and XOR it.
-    *r3 = (1<<5 ^ instruction>>10);     // Only get the bits left of the 10th bit, and XOR it.
-    *funct = (1<<6 ^ instruction);      // Get the first 6 bits of instruction.
-    *offset = (1<<16 ^ instruction);    // Get the first 16 bits of instruction.
-    *jsec = (1<<26 ^ instruction);      // Get the first 26 bits of instruction.
+    *op = (instruction>>26) & 0x0000003f;    // Only get the bits left of the 25th bit.
+    *r1 = (instruction>>21) & 0x1f;     // Only get the bits left of the 20th bit, and XOR it.
+    *r2 = (instruction>>16) & 0x1f;     // Only get the bits left of the 15th bit, and XOR it.
+    *r3 = (instruction>>11) & 0x1f;     // Only get the bits left of the 10th bit, and XOR it.
+    *funct = (instruction) & 0x0000003f;      // Get the first 6 bits of instruction.
+    *offset = (instruction) & 0x0000ffff;    // Get the first 16 bits of instruction.
+    *jsec = (instruction) & 0x03ffffff;      // Get the first 26 bits of instruction.
 
 }
 
@@ -86,7 +86,7 @@ void sign_extend(unsigned offset,unsigned *extended_value){ //Haven't really tes
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
-
+    
 }
 
 /* Read / Write Memory */
